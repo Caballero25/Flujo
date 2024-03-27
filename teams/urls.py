b49c0467e5e5245b3fb2605b14a8d235a4fdfd3a
David Caballero <davidcame124@gmail.com>
2024-03-26 22:58:26 -0500

    Add edit data team view

--- teams/urls.py
+++ teams/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+#/teams/
 urlpatterns = [
     path('', views.getTeams, name="getTeams"),
+    path('edit/<str:pk>/', views.editTeam, name="editTeam"),
 ]
--- teams/views.py
+++ teams/views.py
@@ -7,6 +7,7 @@ from .models import Team
 
 # Create your views here.
 
+@login_required
 def getTeams(request):
     if request.method == "POST":
         teamName = request.POST.get("teamName")
@@ -22,6 +23,38 @@ def getTeams(request):
                 return redirect("getTeams")
             except:
                 messages.error(request, 'Algo salió mal')
-    teams = Team.objects.all().order_by('-created_at')[:3]  
+    teams = Team.objects.filter(leader=request.user).order_by('-created_at')[:3]  
     context = {'teams': teams}      
     return render(request, "templates_teams/main_teams.html", context)
+
+@login_required
+def editTeam(request, pk):
+    try:
+        searchTeam = Team.objects.get(id=pk)
+    except:
+        return redirect("getTeams")
+    if searchTeam.leader != request.user:
+        return redirect("getTeams")
+    if request.method == 'POST':
+        getTeamName = request.POST.get("teamName")
+        getTeamGoal = request.POST.get("teamGoal")
+        getActivity = request.POST.get("teamActivity")
+        if getTeamName == "" or getTeamGoal == "":
+            messages.error(request, 'Los datos no pueden estar vacíos')
+        else:
+            searchTeam.name = getTeamName
+            searchTeam.goal = getTeamGoal
+            if getActivity == "True" or getActivity == "False":
+                active = True if getActivity == "True" else "False"
+                searchTeam.is_active = active
+            searchTeam.save()
+            return redirect("editTeam", pk)
+    opcion1 = {"string": "Activo", "bool": "True"} if searchTeam.is_active == True else {"string": "Inactivo", "bool": "False"}
+    opcion2 = {"string": "Inactivo", "bool": "False"} if searchTeam.is_active == True else {"string": "Activo", "bool": "True"}
+    context = {'team': searchTeam, "opcion1": opcion1, "opcion2": opcion2}
+    return render(request, "templates_teams/edit_team.html", context)
+        
+    
+    
+    
+    
